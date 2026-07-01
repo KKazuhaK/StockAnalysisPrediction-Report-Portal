8fd2008d39df51cd18d39836b4cb74b0a5f8cfa7
Kazuha Mo <lianxltz@gmail.com>
2026-07-01 14:35:04 +0800

    refactor: 图标改为内嵌 SVG 文件目录(可扩展)
    - icons.go 由硬编码 map 改为 //go:embed icons/*.svg + 懒加载缓存
    - 加图标只需丢一个 icons/<name>.svg，零改代码；图标多了也不臃肿

--- icons.go
+++ icons.go
@@ -1,46 +1,43 @@
 package main
 
-import "html/template"
+import (
+	"embed"
+	"html/template"
+	"regexp"
+	"sync"
+)
 
-// Tabler Icons (MIT) 内嵌 SVG，模板用 {{icon "name"}} 调用。
-var iconInner = map[string]string{
-	"refresh":     `<path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" />  <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" />`,
-	"adjustments": `<path d="M4 10a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M6 4v4" /><path d="M6 12v8" /><path d="M10 16a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M12 4v10" /><path d="M12 18v2" /><path d="M16 7a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M18 4v1" /><path d="M18 9v11" />`,
-	"users":       `<path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /><path d="M16 3.13a4 4 0 0 1 0 7.75" /><path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />`,
-	"report-analytics": `<path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
-  <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
-  <path d="M9 17v-5" />
-  <path d="M12 17v-1" />
-  <path d="M15 17v-3" />`,
-	"settings": `<path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
-  <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />`,
-	"tag": `<path d="M7.5 7.5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
-  <path d="M3 6v5.172a2 2 0 0 0 .586 1.414l7.71 7.71a2.41 2.41 0 0 0 3.408 0l5.592 -5.592a2.41 2.41 0 0 0 0 -3.408l-7.71 -7.71a2 2 0 0 0 -1.414 -.586h-5.172a3 3 0 0 0 -3 3z" />`,
-	"files": `<path d="M15 3v4a1 1 0 0 0 1 1h4" />
-  <path d="M18 17h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h4l5 5v7a2 2 0 0 1 -2 2z" />
-  <path d="M16 17v2a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h2" />`,
-	"download": `<path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
-  <path d="M7 11l5 5l5 -5" />
-  <path d="M12 4l0 12" />`,
-	"arrow-left": `<path d="M5 12l14 0" />
-  <path d="M5 12l6 6" />
-  <path d="M5 12l6 -6" />`,
-	"arrow-right": `<path d="M5 12l14 0" />
-  <path d="M13 18l6 -6" />
-  <path d="M13 6l6 6" />`,
-	"trash": `<path d="M4 7l16 0" />
-  <path d="M10 11l0 6" />
-  <path d="M14 11l0 6" />
-  <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-  <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />`,
-	"chevron-left":  `<path d="M15 6l-6 6l6 6" />`,
-	"chevron-right": `<path d="M9 6l6 6l-6 6" />`,
-}
+// 图标 = icons/ 下的 Tabler(MIT) SVG 文件。加图标只需丢一个 <name>.svg 进去，零改代码。
+// 模板里用 {{icon "name"}} 调用；随字号缩放、随文字颜色变。
+//
+//go:embed icons/*.svg
+var iconFS embed.FS
+
+var (
+	iconMu    sync.RWMutex
+	iconCache = map[string]template.HTML{}
+	reSvgBody = regexp.MustCompile(`(?s)<svg[^>]*>(.*)</svg>`)
+	rePlace   = regexp.MustCompile(`<path stroke="none"[^/]*/>`)
+)
 
 func icon(name string) template.HTML {
-	p, ok := iconInner[name]
-	if !ok {
-		return ""
+	iconMu.RLock()
+	c, ok := iconCache[name]
+	iconMu.RUnlock()
+	if ok {
+		return c
 	}
-	return template.HTML(`<svg class="ti" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">` + p + `</svg>`)
+	out := template.HTML("")
+	if b, err := iconFS.ReadFile("icons/" + name + ".svg"); err == nil {
+		if m := reSvgBody.FindSubmatch(b); m != nil {
+			inner := rePlace.ReplaceAll(m[1], nil) // 去掉占位的透明底 path
+			out = template.HTML(`<svg class="ti" width="1em" height="1em" viewBox="0 0 24 24" ` +
+				`fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" ` +
+				`stroke-linejoin="round" aria-hidden="true">` + string(inner) + `</svg>`)
+		}
+	}
+	iconMu.Lock()
+	iconCache[name] = out
+	iconMu.Unlock()
+	return out
 }
